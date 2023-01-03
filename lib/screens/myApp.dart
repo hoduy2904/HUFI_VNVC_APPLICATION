a7b2310d53dc4a8e93d6d825cb822c796fc66919
hoduy2904 <hoduy2904@gmail.com>
2023-01-03 11:25:11 +0700

    fix package, fix out app

---
+++ lib/screens/myApp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeApplication extends StatefulWidget {
+  const HomeApplication({super.key});
+
+  @override
+  State<HomeApplication> createState() => _HomeApplicationState();
+}
+
+class _HomeApplicationState extends State<HomeApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
