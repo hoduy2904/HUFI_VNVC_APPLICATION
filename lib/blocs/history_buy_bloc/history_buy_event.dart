d2ebc4c9d68ab373d70df98e6e947058114c38c3
S3CORP\duy.ho <hoduy2904@gmail.com>
2023-01-18 10:42:19 +0700

    history buy

--- lib/blocs/history_buy_bloc/history_buy_event.dart
+++ lib/blocs/history_buy_bloc/history_buy_event.dart
@@ -5,3 +5,11 @@ class HistoryBuyEvent extends Equatable {
   // TODO: implement props
   List<Object?> get props => [];
 }
+
+class OnLoadHistoryBuyEvent extends HistoryBuyEvent {
+  final String? search;
+  OnLoadHistoryBuyEvent({this.search});
+  @override
+  // TODO: implement props
+  List<Object?> get props => [search];
+}
